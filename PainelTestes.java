36404441a4eb964fbc61c8304328b5141fb10bec
creuma <creumakuzola@gmail.com>
2021-01-06 15:06:32 +0100

    Primeira parte doone
    Fiz o sol, agora faltam os detalhes que estarão a volta dele.

---
+++ PainelTestes.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testes.gui;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author creuma
+ */
+public class PainelTestes extends JPanel implements Runnable  {
+    
+    AffineTransform atf = new AffineTransform();
+    
+    @Override
+    public void paintComponent(Graphics g){
+
+       super.paintComponent(g);
+       Graphics2D g2d = (Graphics2D) g;
+       this.setBackground(new Color(255,255,255));
+       
+       // Anti-Aliasing
+       g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+       
+       atf= g2d.getTransform();
+       g2d.setPaint(new GradientPaint(0,0, Color.YELLOW,61,63, Color.RED,true));
+       g2d.fill(new Ellipse2D.Double(270, 120, 100, 100));
+       
+       g2d.setStroke(new BasicStroke(10));
+       g2d.rotate(40,410,110);
+       g2d.setPaint(Color.orange);
+       g2d.fillRoundRect(415,120, 30, 10,7,7);
+        
+       g2d.setStroke(new BasicStroke(10));
+       g2d.setPaint(Color.ORANGE);
+       g2d.fillRoundRect(375,120, 30, 10,7,7);
+        
+       g2d.setStroke(new BasicStroke(10));
+       
+       g2d.setPaint(Color.ORANGE);
+       g2d.fillRoundRect(335,120, 30, 10,7,7);
+       
+       g2d.setTransform(atf);
+       
+       // Outros
+       atf = new AffineTransform();
+       g2d.setStroke(new BasicStroke(10));
+       g2d.rotate(40,410,90);
+       g2d.setPaint(new GradientPaint(0,0, Color.YELLOW,50,63, Color.RED,true));
+       g2d.fillRoundRect(430,120, 30, 10,7,7);
+        
+       
+       g2d.setStroke(new BasicStroke(10));
+       g2d.rotate(40,410,90);
+       g2d.setPaint(new GradientPaint(0,0, Color.YELLOW,50,63, Color.RED,true));
+       g2d.fillRoundRect(430,120, 30, 10,7,7);
+       
+       /*g2d.setStroke(new BasicStroke(10));
+       g2d.setPaint(Color.ORANGE);
+       g2d.fillRoundRect(375,120, 30, 10,7,7);*/
+       
+       
+       
+        /*
+        for(int i=0; i<6; i++)
+       {
+           g2d.setPaint(new Color(255,255,255));
+           g2d.fillRect(getxPosition(), 305, 80, 10);
+           setxPosition(getxPosition()+110);
+           if(i==5){
+             //  setxPosition(20);
+           }
+       }    
+        */
+    }
+
+    
+    
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
